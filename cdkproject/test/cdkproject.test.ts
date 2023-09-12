f262d6cc52e1c650d9f7d957b07999d4c92e7c8f
Hidenori Miyoshi <miyohide@gmail.com>
2023-09-12 21:08:40 +0900

    implement test

--- cdkproject/test/cdkproject.test.ts
+++ cdkproject/test/cdkproject.test.ts
@@ -1,17 +1,14 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdkproject from '../lib/cdkproject-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Cdkproject from '../lib/cdkproject-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdkproject-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdkproject.CdkprojectStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('VPC Created', () => {
+  const app = new cdk.App();
+  const stack = new Cdkproject.CdkprojectStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::EC2::VPC', {
+    CidrBlock: '10.0.0.0/16',
+    Tags: [{ 'Key': 'Name', 'Value': 'cdkproject-development-vpc'}]
+  });
 });
