8676a31a27efea83c1850afdac5c44e1f62673f7
Hidenori Miyoshi <miyohide@gmail.com>
2023-09-12 21:01:54 +0900

    create VPC

--- cdkproject/lib/cdkproject-stack.ts
+++ cdkproject/lib/cdkproject-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnVPC } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkprojectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkprojectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // VPCを作成する
+    new CfnVPC(this, 'Vpc', {
+      cidrBlock: '10.0.0.0/16',
+      tags: [{ key: 'Name', value: 'cdkproject-development-vpc'}]
+    });
   }
 }
