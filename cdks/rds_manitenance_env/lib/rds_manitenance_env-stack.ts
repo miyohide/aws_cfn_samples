2650c609880858be0d67f9c09a90a9bece10e1bb
Hidenori Miyoshi <miyohide@gmail.com>
2024-04-14 20:50:00 +0900

    サブネット等を作成

--- cdks/rds_manitenance_env/lib/rds_manitenance_env-stack.ts
+++ cdks/rds_manitenance_env/lib/rds_manitenance_env-stack.ts
@@ -1,16 +1,47 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnInstanceConnectEndpoint, Port, SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class RdsManitenanceEnvStack extends cdk.Stack {
+export class MyrdstestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPCとEC2用のPrivate SubnetとDB用のPrivate Subnetを作成する
+    const vpc = new Vpc(this, 'VPC', {
+      cidr: '10.0.0.0/16',
+      vpcName: 'MyRDSTestVPC',
+      natGateways: 0,
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'forEC2',
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS
+        },
+        {
+          cidrMask: 24,
+          name: 'forRDS',
+          subnetType: SubnetType.PRIVATE_ISOLATED
+        }
+      ]
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'RdsManitenanceEnvQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // EC2用のセキュリティグループ
+    const ec2Sg = new SecurityGroup(this, 'ec2Sg', {
+      vpc: vpc,
+    });
+    // EIC用のセキュリティグループ
+    const eicSg = new SecurityGroup(this, 'eicSg', {
+      vpc: vpc,
+      allowAllOutbound: false
+    });
+    ec2Sg.addIngressRule(eicSg, Port.tcp(22));
+    eicSg.addEgressRule(ec2Sg, Port.tcp(22));
+
+    // EC2 Instance Connectを作成する
+    new CfnInstanceConnectEndpoint(this, 'InstanceConnectEndpoint', {
+      subnetId: vpc.selectSubnets({ subnetType: SubnetType.PRIVATE_WITH_EGRESS }).subnetIds[0],
+      securityGroupIds: [eicSg.securityGroupId]
+    });
   }
 }
