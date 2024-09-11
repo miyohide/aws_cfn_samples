1a2313dd86d0491cacbccb2f74228561350c5299
Hidenori Miyoshi <miyohide@gmail.com>
2024-09-11 22:45:04 +0900

    ECRを作成する処理を追加

--- cdks/ecs/lib/ecs-stack.ts
+++ cdks/ecs/lib/ecs-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { Construct } from 'constructs';
 
 export class EcsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps, readonly resourceName = "sample-ecr-app") {
     super(scope, id, props);
 
+    // ECRを作成する
+    const ecr = Repository.fromRepositoryName(
+      this,
+      "EcrRepository",
+      resourceName,
+    );
   }
 }
