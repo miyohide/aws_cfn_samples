86b9809b6756d54f5b756877adf5ba45841a0dba
Hidenori Miyoshi <miyohide@gmail.com>
2024-09-11 22:45:18 +0900

    VPCを作成する処理を追加

--- cdks/ecs/lib/ecs-stack.ts
+++ cdks/ecs/lib/ecs-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { Construct } from 'constructs';
+import { MyVpc } from './construct/my-vpc';
 
 export class EcsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps, readonly resourceName = "sample-ecr-app") {
@@ -12,5 +13,8 @@ export class EcsStack extends cdk.Stack {
       "EcrRepository",
       resourceName,
     );
+
+    // VPCを作成する
+    const myVpc = new MyVpc(this, "VPC", resourceName);
   }
 }
