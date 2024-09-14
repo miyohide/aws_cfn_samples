93a13fac9093c5d9d80da09abf3d3ca7a28d33ea
Hidenori Miyoshi <miyohide@gmail.com>
2024-09-14 22:41:24 +0900

    ALBの作成処理を追加

--- cdks/ecs/lib/ecs-stack.ts
+++ cdks/ecs/lib/ecs-stack.ts
@@ -3,6 +3,7 @@ import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { Construct } from 'constructs';
 import { MyVpc } from './construct/my-vpc';
 import { MySecurityGroup } from './construct/my-security-group';
+import { Alb } from './construct/alb';
 
 export class EcsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps, readonly resourceName = "sample-ecr-app") {
@@ -22,6 +23,14 @@ export class EcsStack extends cdk.Stack {
     const {albSecurityGroup, ecsSecurityGroup, rdsSecurityGroup} = new MySecurityGroup(this, "SecurityGroup", {
       vpc: myVpc.value,
       resourceName,
+    });
+
+    // ALBを作成する
+    const alb = new Alb(this, "Alb", {
+      vpc: myVpc.value,
+      resourceName,
+      securityGroup: albSecurityGroup,
+      subnets: myVpc.getPublicSubnets(),
     });
   }
 }
