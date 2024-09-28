5467ad21fe464648702dcc3eadd23a277c75690b
Hidenori Miyoshi <miyohide@gmail.com>
2024-09-28 20:56:35 +0900

    use tryGetContext

--- cdks/ecr_from_name/lib/ecr_from_name-stack.ts
+++ cdks/ecr_from_name/lib/ecr_from_name-stack.ts
@@ -1,9 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { Construct } from 'constructs';
 
 export class EcrFromNameStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const ecrName = scope.node.tryGetContext("ecrName");
+
+    const ecr = Repository.fromRepositoryName(this, "ECR", ecrName);
+
+    new cdk.CfnOutput(this, "ECR info", {
+      value: ecr.repositoryUri,
+    });
   }
 }
