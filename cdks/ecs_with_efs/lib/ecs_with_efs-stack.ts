271c3b51b3201a6f62b40b00310180ce3c38e25b
Hidenori Miyoshi <miyohide@gmail.com>
2025-02-13 21:34:13 +0900

    EFSをマウントするECS Fargateのサンプル実装

--- cdks/ecs_with_efs/lib/ecs_with_efs-stack.ts
+++ cdks/ecs_with_efs/lib/ecs_with_efs-stack.ts
@@ -1,16 +1,77 @@
 import * as cdk from 'aws-cdk-lib';
+import { Vpc } from 'aws-cdk-lib/aws-ec2/lib/vpc';
+import { Cluster, ContainerDefinition, ContainerImage, FargateTaskDefinition } from 'aws-cdk-lib/aws-ecs';
+import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
+import { FileSystem, LifecyclePolicy, PerformanceMode, ThroughputMode } from 'aws-cdk-lib/aws-efs';
+import { AnyPrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EcsWithEfsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'MyVPC', { maxAzs: 2});
+    const ecsCluster = new Cluster(this, 'EcsCluster', { vpc: vpc });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsWithEfsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fileSystem = new FileSystem(this, 'MyEfsFileSystem', {
+      vpc: vpc,
+      encrypted: true,
+      lifecyclePolicy: LifecyclePolicy.AFTER_14_DAYS,
+      performanceMode: PerformanceMode.GENERAL_PURPOSE,
+      throughputMode: ThroughputMode.BURSTING
+    });
+
+    fileSystem.addToResourcePolicy(
+      new PolicyStatement({
+        actions: ['elasticfilesystem:ClientMount'],
+        principals: [new AnyPrincipal()],
+        conditions: {
+          Bool: {
+            'elasticfilesystem:AccessedViaMountTarget': 'true'
+          }
+        }
+      })
+    );
+
+    const taskDef = new FargateTaskDefinition(this, "MyTaskDef", {
+      memoryLimitMiB: 512,
+      cpu: 256,
+      volumes: [
+        {
+          name: "uploads",
+          efsVolumeConfiguration: {
+            fileSystemId: fileSystem.fileSystemId,
+          }
+        }
+      ]
+    });
+
+    const containerDef = new ContainerDefinition(this, 'MyContainerDefinition', {
+      image: ContainerImage.fromRegistry('coderaiser/cloudcmd'),
+      taskDefinition: taskDef
+    });
+
+    containerDef.addMountPoints(
+      {
+        containerPath: '/uploads',
+        sourceVolume: 'uploads',
+        readOnly: false
+      }
+    );
+
+    containerDef.addPortMappings({
+      containerPort: 8000,
+    });
+
+    const albFargateService = new ApplicationLoadBalancedFargateService(this, 'MyALBService', {
+      cluster: ecsCluster,
+      taskDefinition: taskDef,
+      desiredCount: 2
+    });
+
+    albFargateService.targetGroup.setAttribute('deregistration_delay.timeout_seconds', '30');
+
+    fileSystem.grantRootAccess(albFargateService.taskDefinition.taskRole.grantPrincipal);
+    fileSystem.connections.allowDefaultPortFrom(albFargateService.service.connections);
   }
 }
