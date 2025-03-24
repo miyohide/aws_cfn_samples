ba0a81a8757bfd4d0b4b9d7b07046277f8378339
Hidenori Miyoshi <miyohide@gmail.com>
2025-03-24 22:49:04 +0900

    VPCを作成

--- cdks/rds_with_iam_auth/lib/rds_with_iam_auth-stack.ts
+++ cdks/rds_with_iam_auth/lib/rds_with_iam_auth-stack.ts
@@ -1,8 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
+import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class RdsWithIamAuthStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // VPCを作成する
+    const vpc = new Vpc(this, 'Vpc', {
+      vpcName: 'MyVPC',
+      natGateways: 0,
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          // EC2用
+          cidrMask: 24,
+          name: 'ec2',
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS
+        },
+        {
+          // RDS用
+          cidrMask: 24,
+          name: 'rds',
+          subnetType: SubnetType.PRIVATE_ISOLATED
+        }
+      ]
+    });
   }
 }
