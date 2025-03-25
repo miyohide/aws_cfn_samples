328753cc1949cc1ce2b987f4de81766d3c5cf17c
Hidenori Miyoshi <miyohide@gmail.com>
2025-03-25 21:27:00 +0900

    EC2インスタンスプロファイル用IAMロールを作成し設定

--- cdks/rds_with_iam_auth/lib/rds_with_iam_auth-stack.ts
+++ cdks/rds_with_iam_auth/lib/rds_with_iam_auth-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Instance, InstanceClass, InstanceSize, InstanceType, MachineImage, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { CfnInstanceProfile, ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class RdsWithIamAuthStack extends cdk.Stack {
@@ -31,6 +32,25 @@ export class RdsWithIamAuthStack extends cdk.Stack {
       subnetGroupName: 'ec2'
     });
 
+    // EC2インスタンスプロファイル用IAMロールを作成
+    const ec2Role = new Role(this, "EC2Role", {
+      assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
+      roleName: "EC2Role",
+      description: "IAM Role for EC2"
+    });
+    // IAMロールにSSMのポリシーをアタッチ
+    ec2Role.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName(
+        "AmazonSSMManagedInstanceCore"
+      )
+    );
+    // アクセスログの保存用にCloudWatchのアクセスポリシーをアタッチ
+    ec2Role.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName(
+        "CloudWatchLogsFullAccess"
+      )
+    );
+
     // EC2インスタンスを作成する
     const ec2 = new Instance(this, "MyEC2", {
       instanceType: InstanceType.of(
@@ -39,6 +59,7 @@ export class RdsWithIamAuthStack extends cdk.Stack {
       machineImage: MachineImage.latestAmazonLinux2023(),
       vpc: vpc,
       vpcSubnets: ec2Subnet,
-    })
+      role: ec2Role,
+    });
   }
 }
