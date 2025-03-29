b8f893705ad54aad8aa50125a1c13dfea2ebcc13
Hidenori Miyoshi <miyohide@gmail.com>
2025-03-29 20:58:28 +0900

    enable rds

--- cdks/rds_with_iam_auth/lib/rds_with_iam_auth-stack.ts
+++ cdks/rds_with_iam_auth/lib/rds_with_iam_auth-stack.ts
@@ -163,17 +163,17 @@ export class RdsWithIamAuthStack extends cdk.Stack {
     });
 
     // rdsInstance.grantConnect(ec2Role);
-    // // ec2Role.addToPolicy(
-    // //   new PolicyStatement({
-    // //     actions: [
-    // //       "rds-db:connect"
-    // //     ],
-    // //     resources: [
-    // //       `arn:aws:rds-db:${Aws.REGION}:${Aws.ACCOUNT_ID}:dbuser:${rdsInstance.instanceResourceId}/myiam_db_user`
-    // //     ]
-    // //   })
-    // // );
+    ec2Role.addToPolicy(
+      new PolicyStatement({
+        actions: [
+          "rds-db:connect"
+        ],
+        resources: [
+          `arn:aws:rds-db:${Aws.REGION}:${Aws.ACCOUNT_ID}:dbuser:${rdsInstance.instanceResourceId}/myiam_db_user`
+        ]
+      })
+    );
 
-    // rdsInstance.connections.allowDefaultPortFrom(ec2, "allow connect from ec2");
+    rdsInstance.connections.allowDefaultPortFrom(ec2, "allow connect from ec2");
   }
 }
