eef1fccfe88c770871f09d6c798a3653bfed57cc
Hidenori Miyoshi <miyohide@gmail.com>
2025-03-29 09:40:48 +0900

    use vpc endpoint

--- cdks/rds_with_iam_auth/lib/rds_with_iam_auth-stack.ts
+++ cdks/rds_with_iam_auth/lib/rds_with_iam_auth-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Aws, RemovalPolicy } from 'aws-cdk-lib';
-import { GatewayVpcEndpointAwsService, Instance, InstanceClass, InstanceSize, InstanceType, MachineImage, SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { GatewayVpcEndpointAwsService, Instance, InstanceClass, InstanceSize, InstanceType, InterfaceVpcEndpointAwsService, MachineImage, Port, SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { ManagedPolicy, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Credentials, DatabaseInstance, DatabaseInstanceEngine, SubnetGroup } from 'aws-cdk-lib/aws-rds';
@@ -15,18 +15,19 @@ export class RdsWithIamAuthStack extends cdk.Stack {
     const vpc = new Vpc(this, 'Vpc', {
       vpcName: 'MyVPC',
       maxAzs: 2,
+      natGateways: 0,
       subnetConfiguration: [
-        {
-          // パブリックサブネット用
-          cidrMask: 24,
-          name: 'public',
-          subnetType: SubnetType.PUBLIC
-        },
+        // {
+        //   // パブリックサブネット用
+        //   cidrMask: 24,
+        //   name: 'public',
+        //   subnetType: SubnetType.PUBLIC
+        // },
         {
           // EC2用
           cidrMask: 24,
           name: 'ec2',
-          subnetType: SubnetType.PRIVATE_WITH_EGRESS
+          subnetType: SubnetType.PRIVATE_ISOLATED
         },
         {
           // RDS用
@@ -36,6 +37,35 @@ export class RdsWithIamAuthStack extends cdk.Stack {
         }
       ]
     });
+
+    // EC2用セキュリティグループ
+    const ec2SecurityGroup = new SecurityGroup(this, 'EC2SecurityGroup', {
+      securityGroupName: 'EC2SecurityGroup',
+      vpc: vpc,
+      allowAllOutbound: true
+    });
+
+    // SSMエンドポイント用セキュリティグループ
+    const ssmSecurityGroup = new SecurityGroup(this, 'SSMSecurityGroup', {
+      securityGroupName: 'SSMSecurityGroup',
+      vpc: vpc,
+      allowAllOutbound: true
+    });
+
+    // SSMエンドポイントの設定
+    vpc.addInterfaceEndpoint('SSMEndpoint', {
+      service: InterfaceVpcEndpointAwsService.SSM,
+      securityGroups: [ssmSecurityGroup]
+    });
+    vpc.addInterfaceEndpoint('SSMMessagesEndpoint', {
+      service: InterfaceVpcEndpointAwsService.SSM_MESSAGES,
+      securityGroups: [ssmSecurityGroup]
+    });
+    vpc.addInterfaceEndpoint('EC2MessagesEndpoint', {
+      service: InterfaceVpcEndpointAwsService.EC2_MESSAGES,
+      securityGroups: [ssmSecurityGroup]
+    });
+    ssmSecurityGroup.addIngressRule(ec2SecurityGroup, Port.HTTPS);
 
     const ec2Subnet = vpc.selectSubnets({
       subnetGroupName: 'ec2'
@@ -86,61 +116,61 @@ export class RdsWithIamAuthStack extends cdk.Stack {
       retention: RetentionDays.ONE_DAY
     });
 
-    // RDS用の認証情報
-    const secret = new Secret(this, "MyDBCredentialSecrets", {
-      secretName: "MyDBCredentialSecret",
-      generateSecretString: {
-        secretStringTemplate: JSON.stringify({
-          username: "postgres"
-        }),
-        excludePunctuation: true,
-        includeSpace: false,
-        generateStringKey: "password"
-      },
-    });
+    // // RDS用の認証情報
+    // const secret = new Secret(this, "MyDBCredentialSecrets", {
+    //   secretName: "MyDBCredentialSecret",
+    //   generateSecretString: {
+    //     secretStringTemplate: JSON.stringify({
+    //       username: "postgres"
+    //     }),
+    //     excludePunctuation: true,
+    //     includeSpace: false,
+    //     generateStringKey: "password"
+    //   },
+    // });
 
-    // RDS用セキュリティグループを作成
-    const rdsSG = new SecurityGroup(this, "MySecurityGroupForRDS", {
-      vpc: vpc,
-      allowAllOutbound: true,
-      description: "Security Group for RDS"
-    });
+    // // RDS用セキュリティグループを作成
+    // const rdsSG = new SecurityGroup(this, "MySecurityGroupForRDS", {
+    //   vpc: vpc,
+    //   allowAllOutbound: true,
+    //   description: "Security Group for RDS"
+    // });
 
-    // RDSサブネットグループを作成する
-    const dbSubnetGroup = new SubnetGroup(this, "dbSubnetGroup", {
-      vpc: vpc,
-      subnetGroupName: "MyRDSSubnetGroup",
-      vpcSubnets: dbSubnet,
-      description: "Subnet Group for RDS"
-    });
+    // // RDSサブネットグループを作成する
+    // const dbSubnetGroup = new SubnetGroup(this, "dbSubnetGroup", {
+    //   vpc: vpc,
+    //   subnetGroupName: "MyRDSSubnetGroup",
+    //   vpcSubnets: dbSubnet,
+    //   description: "Subnet Group for RDS"
+    // });
 
-    // RDSインスタンスを作成（PostgreSQL）
-    const rdsInstance = new DatabaseInstance(this, "MyRDSInstance", {
-      instanceIdentifier: "MyRDSInstancePostgreSQL",
-      vpc: vpc,
-      engine: DatabaseInstanceEngine.POSTGRES,
-      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
-      databaseName: "mypostgredb",
-      multiAz: false,
-      subnetGroup: dbSubnetGroup,
-      securityGroups: [rdsSG],
-      removalPolicy: RemovalPolicy.DESTROY,
-      iamAuthentication: true,
-      credentials: Credentials.fromSecret(secret)
-    });
+    // // RDSインスタンスを作成（PostgreSQL）
+    // const rdsInstance = new DatabaseInstance(this, "MyRDSInstance", {
+    //   instanceIdentifier: "MyRDSInstancePostgreSQL",
+    //   vpc: vpc,
+    //   engine: DatabaseInstanceEngine.POSTGRES,
+    //   instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+    //   databaseName: "mypostgredb",
+    //   multiAz: false,
+    //   subnetGroup: dbSubnetGroup,
+    //   securityGroups: [rdsSG],
+    //   removalPolicy: RemovalPolicy.DESTROY,
+    //   iamAuthentication: true,
+    //   credentials: Credentials.fromSecret(secret)
+    // });
 
-    rdsInstance.grantConnect(ec2Role);
-    // ec2Role.addToPolicy(
-    //   new PolicyStatement({
-    //     actions: [
-    //       "rds-db:connect"
-    //     ],
-    //     resources: [
-    //       `arn:aws:rds-db:${Aws.REGION}:${Aws.ACCOUNT_ID}:dbuser:${rdsInstance.instanceResourceId}/myiam_db_user`
-    //     ]
-    //   })
-    // );
+    // rdsInstance.grantConnect(ec2Role);
+    // // ec2Role.addToPolicy(
+    // //   new PolicyStatement({
+    // //     actions: [
+    // //       "rds-db:connect"
+    // //     ],
+    // //     resources: [
+    // //       `arn:aws:rds-db:${Aws.REGION}:${Aws.ACCOUNT_ID}:dbuser:${rdsInstance.instanceResourceId}/myiam_db_user`
+    // //     ]
+    // //   })
+    // // );
 
-    rdsInstance.connections.allowDefaultPortFrom(ec2, "allow connect from ec2");
+    // rdsInstance.connections.allowDefaultPortFrom(ec2, "allow connect from ec2");
   }
 }
