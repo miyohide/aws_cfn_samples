9be2cd378ea05731dca75d7b726146325a090c46
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-17 10:23:48 +0900

    create sample app

--- cdks/lambda_with_test/lib/lambda_with_test-stack.ts
+++ cdks/lambda_with_test/lib/lambda_with_test-stack.ts
@@ -4,5 +4,28 @@ import { Construct } from 'constructs';
 export class LambdaWithTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // AWS CDKを使ってRubyランタイムのLambda関数を作成する
+    const lambdaFunction = new cdk.aws_lambda.Function(this, 'RubyLambda', {
+      runtime: cdk.aws_lambda.Runtime.RUBY_3_4,
+      handler: 'index.handler',
+      code: cdk.aws_lambda.Code.fromInline(`
+require 'json'
+
+def handler(event:, context:)
+  time = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+  response = {
+    "statusCode": 200,
+    "body": JSON.generate({
+      message: 'Hello from Lambda!',
+      time: time
+    })
+  }
+  response
+end
+      `),
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(30),
+    });
   }
 }
