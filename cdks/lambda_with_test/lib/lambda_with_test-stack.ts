e38fcb80d9a9c8384a45e48d4950b6f87cd04ae7
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-17 10:36:14 +0900

    create test event

--- cdks/lambda_with_test/lib/lambda_with_test-stack.ts
+++ cdks/lambda_with_test/lib/lambda_with_test-stack.ts
@@ -1,12 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnSchema } from 'aws-cdk-lib/aws-eventschemas';
 import { Construct } from 'constructs';
 
 export class LambdaWithTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const lambdaName = 'ruby-lambda-with-test'
+
     // AWS CDKを使ってRubyランタイムのLambda関数を作成する
     const lambdaFunction = new cdk.aws_lambda.Function(this, 'RubyLambda', {
+      functionName: lambdaName,
       runtime: cdk.aws_lambda.Runtime.RUBY_3_4,
       handler: 'index.handler',
       code: cdk.aws_lambda.Code.fromInline(`
@@ -27,5 +31,42 @@ end
       memorySize: 128,
       timeout: cdk.Duration.seconds(30),
     });
+
+    // testEventを作成
+    new CfnSchema(this, `${id}-CfnSchema`, {
+      registryName: 'lambda-testevent-schemas',
+      type: 'OpenApi3',
+      schemaName: `_${lambdaName}-schema`,
+      content: JSON.stringify({
+        "openapi": "3.0.0",
+        "info": {
+          "version": "1.0.0",
+          "title": "TestEvent"
+        },
+        "paths": {},
+        "components": {
+          "schemas": {
+            "Event": {
+              "type": "object",
+              "properties": {
+                "message": {
+                  "type": "string"
+                }
+              },
+              "required": [
+                "message"
+              ]
+            }
+          },
+          examples: {
+            "sample1": {
+              "value": {
+                "message": "Hello from Lambda!"
+              }
+            }
+          }
+        }
+      })
+    })
   }
 }
